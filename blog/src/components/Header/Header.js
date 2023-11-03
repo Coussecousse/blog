166c2405ddfe7c14adbf663a1a8108f6a4417f9d
Erika <adob.e@hotmail.com>
2023-11-03 16:54:51 +0100

    feat: header style

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -9,19 +9,25 @@ import logo from '../../images/logo.svg'
 
 export default function Header() {
     return (
-        <header style={{"background": "red"}}>
-            <a href="" ><img src={logo} alt="Little Star Logo" /></a>
-            <button></button>
-            <nav>
-                <ul>
-                    {/* <li><NavLink to={paths.HOME}>Accueil</NavLink></li>
-                    <li><NavLink to={paths.PLAYLISTS}>Playlists</NavLink></li> */}
-                    <li><a href={paths.HOME}>Accueil</a></li>
-                    <li><a href={paths.PLAYLISTS}>Playlist</a></li>
-                </ul>
-            </nav>
-            <button><i className="fa-solid fa-magnifying-glass"></i></button>
-            {/* A transformer en form ? */}
+        <header className={styles.header} >
+            <div className={styles.navContainer}>
+                <div className={styles.topContainer}>
+                    <a href={paths.HOME} ><img src={logo} alt="Little Star Logo" className={styles.logo} /></a>
+                    <button className={styles.mobileButton}><span></span><span></span></button>
+                </div>
+                <div className={styles.bottomContainer}>
+                    <nav>
+                        <ul>
+                            {/* <li><NavLink to={paths.HOME}>Accueil</NavLink></li>
+                            <li><NavLink to={paths.PLAYLISTS}>Playlists</NavLink></li> */}
+                            <li><a href={paths.HOME}>Accueil</a></li>
+                            <li><a href={paths.PLAYLISTS}>Playlist</a></li>
+                        </ul>
+                    </nav>
+                    <button className={styles.search}><i className="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+                {/* A transformer en form ? */}
+            </div>
         </header>
     )
 }
